12c1f9b587ea0551e01b0d5e441e91bf63e2e707
surgosan <sergy200118@gmail.com>
2023-08-21 18:19:47 -0400

    Account Controller and routes

--- Server/controllers/AccountController.js
+++ Server/controllers/AccountController.js
@@ -6,9 +6,69 @@ module.exports = {
     {
         try {
             const newAccount = await Account.create(req.body);
-            res.send(`${Account.name} has been added to accounts`);
+            res.send(`${newAccount.name} has been added to accounts`);
         } catch (error) {
             const message = 'An error has occurred';
+            errors.errorHandler(res, error, message);
+        }
+    },
+
+    async deleteAccount (req, res)
+    {
+        try {
+            const { id } = req.body;
+            await Account.destroy({ where: { id } });
+            res.send('Account removed.')
+        } catch (error) {
+            const message = 'Could not delete account.';
+            errors.errorHandler(res, error, message);
+        }
+    },
+
+    async displayAccounts(req, res)
+    {
+        try {
+            const accounts = await Account.findAll();
+
+            if(accounts.length === 0) {
+                const error = errors.notFound('No accounts to display.');
+                res.status(error.status).send({
+                    error: error.message
+                })
+            } else {res.send(accounts)}
+        } catch (error) {
+            const message = 'Could not display accounts.';
+            errors.errorHandler(res, error, message);
+        }
+    },
+
+    async fetchAccount (req, res)
+    {
+        try {
+            const { id } = req.body;
+            const account = await Account.findByPk(id);
+            res.send(account);
+        } catch (error) {
+            const message = 'Could not fetch account.';
+            errors.errorHandler(res, error, message);
+        }
+    },
+
+    async editAccount (req, res) {
+        try {
+            const { id } = req.body;
+            const account = await Account.findByPk(id);
+
+            if(account) {
+                await Account.update(req.body, {
+                    where: {id: id}
+                });
+                res.send('Account updated successfully.');
+            } else {
+                res.send('Account not found.')
+            }
+        } catch (error) {
+            const message = 'Could not edit account.';
             errors.errorHandler(res, error, message);
         }
     }
--- Server/routes.js
+++ Server/routes.js
@@ -1,5 +1,9 @@
 const AccountController = require('./controllers/AccountController');
 
 module.exports = (app) => {
+    app.get('/account/display', AccountController.displayAccounts);
     app.post('/account/new', AccountController.newAccount);
+    app.post('/account/fetch', AccountController.fetchAccount);
+    app.delete('/account/destroy', AccountController.deleteAccount);
+    app.put('/account/edit', AccountController.editAccount);
 }
